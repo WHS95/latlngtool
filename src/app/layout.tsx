ffaeb4fec7a0f71123d589d54f07f71ce5242305
WHS95 <tjdngur22@gmail.com>
2025-06-23 01:12:30 +0900

    ✨vercel 분석 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { getMetadata } from "@/lib/metadata";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -82,6 +84,8 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
